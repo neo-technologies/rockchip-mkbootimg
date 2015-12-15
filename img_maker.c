ca5bfcaecb47920546e902b0bc622b15e45639e3
cyrozap <cyrozap@gmail.com>
2015-12-14 22:25:36 -0500

    Whitespace fixes

--- img_maker.c
+++ img_maker.c
@@ -13,12 +13,12 @@ unsigned int import_data(const char* infile, void *head, size_t head_len, FILE *
 	FILE *in_fp = NULL;
 	unsigned readlen = 0;
 	unsigned char buffer[1024];
-	
+
 	in_fp = fopen(infile, "rb");
 
 	if (!in_fp)
 		goto import_end;
-		
+
 	readlen = fread(head, 1, head_len, in_fp);
 	if (readlen)
 	{
@@ -51,10 +51,10 @@ void append_md5sum(FILE *fp)
 	MD5_CTX md5_ctx;
 	unsigned char buffer[1024];
 	int i;
-	
+
 	MD5_Init(&md5_ctx);
 	fseek(fp, 0, SEEK_SET);
-	
+
 	while (1)
 	{
 		int len = fread(buffer, 1, sizeof(buffer), fp);
@@ -62,13 +62,13 @@ void append_md5sum(FILE *fp)
 		{
 			MD5_Update(&md5_ctx, buffer, len);
 		}
-		
+
 		if (len != sizeof(buffer))
 			break;
 	}
-	
+
 	MD5_Final(buffer, &md5_ctx);
-	
+
 	for (i = 0; i < 16; ++i)
 	{
 		fprintf(fp, "%02x", buffer[i]);
@@ -86,7 +86,7 @@ int pack_rom(unsigned int chiptype, const char *loader_filename, int majver, int
 		.head_len = 0x66,
 		.loader_offset = 0x66
 	};
-	
+
 	struct update_header rkaf_header;
 	struct bootloader_header loader_header;
 
@@ -126,7 +126,7 @@ int pack_rom(unsigned int chiptype, const char *loader_filename, int majver, int
 		(rom_header.version >> 16) & 0xFF,
 		(rom_header.version) & 0xFFFF);
 
-	printf("build time: %d-%02d-%02d %02d:%02d:%02d\n", 
+	printf("build time: %d-%02d-%02d %02d:%02d:%02d\n",
 		rom_header.year, rom_header.month, rom_header.day,
 		rom_header.hour, rom_header.minute, rom_header.second);
 
@@ -135,13 +135,13 @@ int pack_rom(unsigned int chiptype, const char *loader_filename, int majver, int
 	fseek(fp, rom_header.loader_offset, SEEK_SET);
 	fprintf(stderr, "generate image...\n");
 	rom_header.loader_length = import_data(loader_filename, &loader_header, sizeof(loader_header), fp);
-	
+
 	if (rom_header.loader_length <  sizeof(loader_header))
 	{
 		fprintf(stderr, "invalid loader :\"\%s\"\n",  loader_filename);
 		goto pack_fail;
 	}
-	
+
 	rom_header.image_offset = rom_header.loader_offset + rom_header.loader_length;
 	rom_header.image_length = import_data(image_filename, &rkaf_header, sizeof(rkaf_header), fp);
 	if (rom_header.image_length < sizeof(rkaf_header))
@@ -149,22 +149,22 @@ int pack_rom(unsigned int chiptype, const char *loader_filename, int majver, int
 		fprintf(stderr, "invalid rom :\"\%s\"\n",  image_filename);
 		goto pack_fail;
 	}
-	
+
 	rom_header.unknown2 = 1;
-	
+
 	rom_header.system_fstype = 0;
-	
+
 	for (i = 0; i < rkaf_header.num_parts; ++i)
 	{
 		if (strcmp(rkaf_header.parts[i].name, "backup") == 0)
 			break;
 	}
-	
+
 	if (i < rkaf_header.num_parts)
 		rom_header.backup_endpos = (rkaf_header.parts[i].nand_addr + rkaf_header.parts[i].nand_size) / 0x800;
 	else
 		rom_header.backup_endpos = 0;
-	
+
 	fseek(fp, 0, SEEK_SET);
 	if (1 != fwrite(&rom_header, sizeof(rom_header), 1, fp))
 		goto pack_fail;
@@ -191,7 +191,7 @@ void usage(const char *appname) {
 			"%s -rk30 Loader.bin 1 0 23 rawimage.img rkimage.img \tRK30 board\n"
 			"%s -rk31 Loader.bin 4 0 4 rawimage.img rkimage.img \tRK31 board\n"
 			"%s -rk32 Loader.bin 4 4 2 rawimage.img rkimage.img \tRK32 board\n"
-                        "%s -rk3368 Loader.bin 5 0 0 rawimage.img rkimage.img \tRK3368 board\n"
+			"%s -rk3368 Loader.bin 5 0 0 rawimage.img rkimage.img \tRK3368 board\n"
 			"\n\n"
 			"Options:\n"
 			"[chiptype]:\n\t-rk29\n\t-rk30\n\t-rk31\n\t-rk32\n", p, p, p, p);
